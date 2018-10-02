49ed94a7cd55008b7cc1db1fa51f8d92bc0f0665
Zhen Xia <43132113+zhenxia1012@users.noreply.github.com>
2018-10-02 00:13:27 -0700

    Add files via upload

---
+++ TestCode1/TestCode1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDemo
+{
+    public class CodeToTest1 : MarshalByRefObject
+    {
+        public void annunciator(string msg)
+        {
+            Console.Write("\n    Self-Indroduciton: {0}", msg);
+        }
+    }
+}
---
+++ TestDriver2/TestDriver2.cs
@@ -0,0 +1,61 @@
+﻿/////////////////////////////////////////////////////////////////////
+// TestDriver2.cs - define a test to run                           //
+//                                                                 //
+/*
+*   Test driver needs to know the types and their interfaces
+*   used by the code it will test.  It doesn't need to know
+*   anything about the test harness.
+*/
+#define TEST2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHarness;
+
+namespace TestDemo
+{
+    public class TestDriver2 : MarshalByRefObject, ITest
+    {
+        private CodeToTest2 code;  // will be compiled into separate DLL
+
+        //----< Testdriver constructor >---------------------------------
+        /*
+        *  For production code the test driver may need the tested code
+        *  to provide a creational function.
+        */
+        public TestDriver2()
+        {
+            code = new CodeToTest2();
+        }
+        //----< description of this test >-------------------------------
+
+        public string getLog()
+        {
+            return "demo a successful test of two integers division: 9/3";
+        }
+        //----< test method is where all the testing gets done >---------
+
+        public bool test()
+        {
+            code.annunciator(9, 3);
+            return true;
+        }
+    }
+
+#if (TEST2)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDriver2 driver = new TestDriver2();
+    
+            driver.test();
+            Console.Write(driver.getLog());
+            Console.ReadKey();
+
+        }
+    }
+#endif
+}
